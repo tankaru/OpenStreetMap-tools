03bf9a840aa81c2f30ee13e1633ff5a65df755ea
tankaru <muramototomoya@gmail.com>
2020-03-03 21:48:56 +0900

    routefinder added

---
+++ utils.js
@@ -0,0 +1,105 @@
+
+//https://coderwall.com/p/zrlulq/parsing-a-link-header-in-javascript
+// Unquote string (utility)
+function unquote(value) {
+    if (value.charAt(0) == '"' && value.charAt(value.length - 1) == '"') return value.substring(1, value.length - 1);
+    return value;
+}
+
+// Parse a Link header
+function parseLinkHeader(header) {
+    var linkexp = /<[^>]*>\s*(\s*;\s*[^\(\)<>@,;:"\/\[\]\?={} \t]+=(([^\(\)<>@,;:"\/\[\]\?={} \t]+)|("[^"]*")))*(,|$)/g;
+    var paramexp = /[^\(\)<>@,;:"\/\[\]\?={} \t]+=(([^\(\)<>@,;:"\/\[\]\?={} \t]+)|("[^"]*"))/g;
+
+    var matches = header.match(linkexp);
+    var rels = new Object();
+    for (i = 0; i < matches.length; i++) {
+        var split = matches[i].split('>');
+        var href = split[0].substring(1);
+        var ps = split[1];
+        var link = new Object();
+        link.href = href;
+        var s = ps.match(paramexp);
+        for (j = 0; j < s.length; j++) {
+            var p = s[j];
+            var paramsplit = p.split('=');
+            var name = paramsplit[0];
+            link[name] = unquote(paramsplit[1]);
+        }
+
+        if (link.rel != undefined) {
+            rels[link.rel] = link;
+        }
+    }
+
+    return rels;
+}
+//-------------
+
+
+function decodePath(encoded, is3D) {
+    var len = encoded.length;
+    var index = 0;
+    var array = [];
+    var lat = 0;
+    var lng = 0;
+    var ele = 0;
+
+    while (index < len) {
+        var b;
+        var shift = 0;
+        var result = 0;
+        do {
+            b = encoded.charCodeAt(index++) - 63;
+            result |= (b & 0x1f) << shift;
+            shift += 5;
+        } while (b >= 0x20);
+        var deltaLat = ((result & 1) ? ~(result >> 1) : (result >> 1));
+        lat += deltaLat;
+
+        shift = 0;
+        result = 0;
+        do {
+            b = encoded.charCodeAt(index++) - 63;
+            result |= (b & 0x1f) << shift;
+            shift += 5;
+        } while (b >= 0x20);
+        var deltaLon = ((result & 1) ? ~(result >> 1) : (result >> 1));
+        lng += deltaLon;
+
+        if (is3D) {
+            // elevation
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charCodeAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            var deltaEle = ((result & 1) ? ~(result >> 1) : (result >> 1));
+            ele += deltaEle;
+            array.push([lng * 1e-5, lat * 1e-5, ele / 100]);
+        } else
+            array.push([lng * 1e-5, lat * 1e-5]);
+    }
+    // var end = new Date().getTime();
+    // console.log("decoded " + len + " coordinates in " + ((end - start) / 1000) + "s");
+    return array;
+};
+
+function getLatLonZoom(url) {
+  const match = url.match(/www\.mapillary\.com\/app\/\?lat=(-?\d[0-9.]*)&lng=(-?\d[0-9.]*)&z=(\d{1,2})/);
+  if (match) {
+	const [, lat, lon,zoom] = match;
+	return [lat, lon, zoom];
+  }
+}
+function latLonZoomToBbox(lat, lon, zoom) {
+	const part = Math.pow(0.5,zoom);
+	const minlon = Number(lon) - 360*part/2;
+	const maxlon = Number(lon) + 360*part/2;
+	const minlat = Number(lat) - 180*part/2;
+	const maxlat = Number(lat) + 180*part/2;
+	return [minlon, minlat, maxlon, maxlat];
+
+}
